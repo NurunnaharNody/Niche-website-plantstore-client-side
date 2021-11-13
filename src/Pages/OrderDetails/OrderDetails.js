31f1dc3e1a5625903f7026a0b1abfe19fe83c230
Nurunnahar Nody <nurunnaharnody@gmail.com>
2021-11-13 13:21:41 +0600

    updated

--- src/Pages/OrderDetails/OrderDetails.js
+++ src/Pages/OrderDetails/OrderDetails.js
@@ -4,6 +4,9 @@ import { useParams } from 'react-router';
 import { styled, Box } from '@mui/system';
 import ModalUnstyled from '@mui/core/ModalUnstyled';
 import './OrderDetails.css'
+import useAuth from '../../hooks/useAuth';
+import { useForm } from "react-hook-form";
+import axios from 'axios';
 
 const StyledModal = styled(ModalUnstyled)`
   position: fixed;
@@ -43,12 +46,25 @@ const OrderDetails = () => {
      const [open, setOpen] = React.useState(false);
      const handleOpen = () => setOpen(true);
      const handleClose = () => setOpen(false);
-
+     const {user} = useAuth();
+     const { register, handleSubmit, reset } = useForm();
+     
     useEffect( () =>{
         fetch(`https://agile-hollows-82713.herokuapp.com/services/${serviceId}`)
         .then(res => res.json())
         .then(data => setService(data));
     }, [])
+
+    const onSubmit = data => {
+      console.log(data)
+      axios.post('https://agile-hollows-82713.herokuapp.com/orders', data)
+      .then(res => {
+          if(res.data.insertedId){
+              alert('Added your Order to the cart Successfully');
+              reset();
+          }
+      })
+  };
     return (
         <Grid item xs={2} sm={12} md={12}>
         <Card sx={{  border:0,  boxShadow: 0}}>
@@ -82,8 +98,21 @@ const OrderDetails = () => {
         BackdropComponent={Backdrop}
       >
         <Box sx={style} className="modal-details">
-          <h2 id="unstyled-modal-title">Placed Order Successfully</h2>
-          <p id="unstyled-modal-description">Thanks For Being With us.!! 
+          <h2 id="unstyled-modal-title">Please Give this information to confirm your order.!</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("name", { required: true, maxLength: 20 })}
+        value={service.name} /> <br/>
+          <input {...register("id")}
+        value={service._id} /> <br/>
+    <input {...register("username", { required: true, maxLength: 20 })}
+        value={user.displayName} /> <br/>
+        <input {...register("email", { required: true, maxLength: 20 })}
+        value={user.email} /> <br/>
+    <textarea {...register("location")} placeholder="Your Location Please!!"/> <br/>
+    <input type="number" {...register("count", { min: 0, max: 5 })} placeholder="How Much?" /> <br/>
+          <input className="add-review-button" type="submit" />
+    </form>
+      <p id="unstyled-modal-description">Thanks For Being With us.!! 
           Click anywhere to go back.</p>
         </Box>
       </StyledModal>
